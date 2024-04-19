3eae686dc72fe8211d68b824726ea2c42e68e41f
Om21610018 <omkarpenshanwar346@gmail.com>
2024-04-20 00:06:43 +0530

    updated ui and added animations

--- multicore-courseware-frontend/src/screens/HomeScreen/HomeScreen.jsx
+++ multicore-courseware-frontend/src/screens/HomeScreen/HomeScreen.jsx
@@ -1,86 +1,89 @@
 import Course from "../../components/Course";
-import Hero from "../../components/Hero";
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
-import { useSelector } from 'react-redux';
-
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { motion } from "framer-motion";
 
 const HomeScreen = () => {
+  const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
-    const baseUrl = process.env.REACT_APP_API_BASE_URL;
+  const [courses, setCourses] = useState([]);
+  const [enrolledCourses, setEnrolledCourses] = useState([]);
 
-    const [courses, setCourses] = useState([]);
-    const [enrolledCourses, setEnrolledCourses] = useState([]);
+  const { accessToken } = useSelector((state) => state.auth);
 
-    const { accessToken } = useSelector(
-        state => state.auth
-    );
+  useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        // Check if access token exists
+        if (!accessToken) {
+          console.error("Access token not found in local storage");
+          return;
+        }
 
-    useEffect(() => {
-        const fetchCourses = async () => {
-            try {
-
-                // Check if access token exists
-                if (!accessToken) {
-                    console.error('Access token not found in local storage');
-                    return;
-                }
-
-                // Set the authorization header with the access token
-                const headers = {
-                    'Authorization': `Bearer ${accessToken}`
-                };
-
-                // Make the HTTP request with the authorization header
-                const response = await axios.get(`${baseUrl}/courses/course-list/`, { headers });
-                setCourses(response.data.data);
-            } catch (error) {
-                console.error('Error fetching courses:', error);
-            }
+        // Set the authorization header with the access token
+        const headers = {
+          Authorization: `Bearer ${accessToken}`,
         };
 
-        fetchCourses();
-    }, [baseUrl]);
-
-
-    useEffect(() => {
-        const fetchEnrolledCourses = async () => {
-            try {
-                // Check if access token exists
-                if (!accessToken) {
-                    console.error('Access token not found in local storage');
-                    return;
-                }
-
-                // Set the authorization header with the access token
-                const headers = {
-                    'Authorization': `Bearer ${accessToken}`
-                };
-
-                // Make the HTTP request with the authorization header
-                const response = await axios.get(`${baseUrl}/courses/enrollments-list-detail/`, { headers });
-                setEnrolledCourses(response.data);
-            } catch (error) {
-                console.error('Error fetching courses:', error);
-            }
-        };
-
-        fetchEnrolledCourses();
-    }, [baseUrl]);
-
-    const isCourseEnrolled = (courseId) => {
-        return enrolledCourses.some(course => course.course === courseId);
+        // Make the HTTP request with the authorization header
+        const response = await axios.get(`${baseUrl}/courses/course-list/`, {
+          headers,
+        });
+        setCourses(response.data.data);
+      } catch (error) {
+        console.error("Error fetching courses:", error);
+      }
     };
 
-    return (
-        <div className=" flex gap-x-5 gap-y-5  mt-6">
-            {courses.map((course) => (
-                <Course key={course.id}
-                    course={course}
-                    isEnrolled={isCourseEnrolled(course.id)}
-                />
-            ))}
-        </div>
-    );
+    fetchCourses();
+  }, [baseUrl]);
+
+  useEffect(() => {
+    const fetchEnrolledCourses = async () => {
+      try {
+        // Check if access token exists
+        if (!accessToken) {
+          console.error("Access token not found in local storage");
+          return;
+        }
+
+        // Set the authorization header with the access token
+        const headers = {
+          Authorization: `Bearer ${accessToken}`,
+        };
+
+        // Make the HTTP request with the authorization header
+        const response = await axios.get(
+          `${baseUrl}/courses/enrollments-list-detail/`,
+          { headers }
+        );
+        setEnrolledCourses(response.data);
+      } catch (error) {
+        console.error("Error fetching courses:", error);
+      }
+    };
+
+    fetchEnrolledCourses();
+  }, [baseUrl]);
+
+  const isCourseEnrolled = (courseId) => {
+    return enrolledCourses.some((course) => course.course === courseId);
+  };
+
+  return (
+    <div className=" flex flex-wrap  gap-x-5 gap-y-5  mt-6">
+      {courses.map((course, index) => (
+        <motion.div
+          key={course.id}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: index * 0.1 }}
+        >
+          <Course course={course} isEnrolled={isCourseEnrolled(course.id)} />
+        </motion.div>
+      ))}
+    </div>
+  );
 };
 export default HomeScreen;
--- multicore_courseware_backend/multicore_courseware_backend/settings.py
+++ multicore_courseware_backend/multicore_courseware_backend/settings.py
@@ -146,7 +146,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',  # Add your frontend URL here
     'http://127.0.0.1:3000',
-    'http://localhost:10028',
+    'http://localhost:12443',
     # Add more origins if needed
 ]
 
